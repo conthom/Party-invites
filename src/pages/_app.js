ec4115a222a59a721e004b5598d2b8ce03a0e6fe
conthom <conthom@iu.edu>
2025-05-13 12:45:55 -0500

    test url integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,22 @@
+import Head from 'next/head';
 import "../styles/styles.css";
-import React from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Party Invites</title>
+        <meta name="description" content="RSVP to our upcoming party!" />
+        <meta property="og:title" content="Party Invites" />
+        <meta property="og:description" content="RSVP to our upcoming party!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/house.png" /> {/* You'll need to add this image */}
+        <meta property="og:url" content="https://partyinv.vercel.app" />
+        <meta name="twitter:card" content="house.png" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/house.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
